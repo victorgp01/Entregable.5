b03adf624e4991a1625ba59c214204fd9b82680d
victorgp01 <victorgonzalezpresinal@gmail.com>
2022-12-20 20:17:56 -0400

    modific card

--- src/components/pokedex/PokeCard.jsx
+++ src/components/pokedex/PokeCard.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles/pokeCard.css";
+import "./styles/pokecard.css";
 
 const PokeCard = ({ url }) => {
   const [pokemon, setPokemon] = useState();
